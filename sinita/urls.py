ec56a5028c7d6bf32f37c173410e010742e07632
dias <diorgelesdias@gmail.com>
2017-11-09 21:38:24 -0300

    adiocionado url, feito o adicionar tarefas e migrate

--- sinita/urls.py
+++ sinita/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from tarefas.views import hello
+from tarefas.views import hello, task
 
 urlpatterns = [
-    url(r'^', hello),
+    url(r'^$', hello),
+    url(r'^tarefas', task)
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
+from tarefas.models import Tarefas
 
 def hello(request):
-    return HttpResponse('Olá mundo!')
+    return HttpResponse('Olá mundo2!')
+
+def adicionar():
+    tarefa = Tarefas()
+    tarefa.nome = "teste2222"
+    tarefa.descricao = "test2"
+    tarefa.save()
+
+def task(request):
+    adicionar()  
+    print(Tarefas.objects.all())  
+    return HttpResponse('Olá tarefas!') 
